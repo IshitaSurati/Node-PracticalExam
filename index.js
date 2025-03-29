40b277241dfeeb803469fadc1b11eb9b9af24cb7
Ishitasurat25 <ishitasurati83@gmail.com>
2025-03-29 23:48:28 +0530

    update

--- index.js
+++ index.js
@@ -1,24 +1,37 @@
+// server.js
+require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const authRoutes = require('./routes/authRoutes');
+const blogRoutes = require('./routes/blogRoutes');
+const path = require('path');
 const DBconnect = require('./config/db');
+
 const app = express();
-app.use(express.json())
+const PORT = process.env.PORT || 3000;
 
 
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
+// Routes
+app.use('/auth', authRoutes);
+app.use('/blogs', blogRoutes);
 
+// Home Route
+app.get('/', (req, res) => {
+    res.render('articleList');
+});
 
-
-
-app.get("/", (req, res) => {
-    res.send("Welcome To My Blog Project API Backend......☺☺🙋‍♀️")
-})
-
-
-
-
-
+// Server Listening
 app.listen(8090, () => {
-    console.log("Server Started....");
-})
+    console.log(`Server running on http://localhost:8090`);
+});
 DBconnect();

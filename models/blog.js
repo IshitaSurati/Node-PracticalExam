2a9270775ad80f5c922217db759d427bbf3cb9a6
Ishitasurat25 <ishitasurati83@gmail.com>
2025-03-29 23:25:16 +0530

    models created

---
+++ models/blog.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const blogSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+  author: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  comments: [{
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    text: { type: String },
+    createdAt: { type: Date, default: Date.now }
+  }],
+  likes: { type: Number, default: 0 },
+  createdAt: { type: Date, default: Date.now }
+});
+
+const Blog = mongoose.model('Blog', blogSchema);
+
+module.exports = Blog;
